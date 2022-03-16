3e3dfbef2302122c01584977fee8fdd918cb9447
Alejandro <alex_carrasco17@hotmail.com>
2022-03-16 16:59:01 -0500

    Updated Version.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import "./App.css";
-import SignIn from "./pages/SignIn";
 import "react-toastify/dist/ReactToastify.css";
-
+import Home from "../src/pages/Home";
+import AddEdit from "../src/pages/AddEdit";
+import View from "../src/pages/View";
+import About from "../src/pages/About";
+import Header from "../src/components/Header";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <>
-      <SignIn />
+      <BrowserRouter>
+        <div className="App">
+          <Header />
+          <ToastContainer position="top-center" />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/add" component={AddEdit} />
+            <Route path="/update/:id" component={AddEdit} />
+            <Route path="/view/:id" component={View} />
+            <Route path="/about" component={About} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     </>
   );
 }
